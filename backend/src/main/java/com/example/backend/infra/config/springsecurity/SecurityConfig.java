384aa175632c7f5f696ceffa44d56d3ddd7c968f
Raphile14 <rllddalangin@gmail.com>
2023-02-21 17:02:23 +0800

    feat(backend security): initial security setup

---
+++ backend/src/main/java/com/example/backend/infra/config/springsecurity/SecurityConfig.java
@@ -0,0 +1,62 @@
+package com.example.backend.infra.config.springsecurity;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+  /**
+   * Configure Spring Security
+   * 
+   * @param http
+   * @return
+   * @throws Exception
+   */
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    return http
+        .cors().and().csrf().disable()
+        .authorizeHttpRequests(auth -> auth
+            .requestMatchers("/login", "/register").permitAll()
+            .anyRequest().authenticated())
+        // .formLogin().loginPage("/login")
+        // .and()
+        // .logout().logoutUrl("/logout").logoutSuccessUrl("/login")
+        // .and()
+        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        // TODO: Add JwtRequestFilter
+        .build();
+  }
+
+  /**
+   * Authentication manager
+   * 
+   * @param configuration
+   * @return
+   * @throws Exception
+   */
+  @Bean
+  public AuthenticationManager authenticationManager(final AuthenticationConfiguration configuration) throws Exception {
+    return configuration.getAuthenticationManager();
+  }
+
+  /**
+   * Password encoder
+   * 
+   * @return
+   */
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+}
---
+++ backend/src/main/java/com/example/backend/infra/config/springsecurity/handler/AuthFailHandler.java
@@ -0,0 +1,5 @@
+package com.example.backend.infra.config.springsecurity.handler;
+
+public class AuthFailHandler {
+
+}
