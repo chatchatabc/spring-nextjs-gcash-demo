f70675db4f1c1fd9536bba3934847542e8d84df0
Raphile14 <rllddalangin@gmail.com>
2023-02-27 17:28:46 +0800

    chore(frontend): improved product card design

--- frontend/src/components/product/ListProducts.tsx
+++ frontend/src/components/product/ListProducts.tsx
@@ -27,16 +27,20 @@ export default function ListProducts(params: {
         {products.map((product) => (
           <div
             key={`product_${product.id}`}
-            className='my-4 border border-gray-500 rounded-md p-8 mx-8'
+            className='my-4 h-64 border flex border-gray-500 rounded-3xl overflow-hidden mx-8'
           >
-            <div className='grid grid-cols-2'>
-              <img src={product.imageUrl} alt={product.name} />
-              <div>
-                <h1 className='text-xl font-semibold'>Name: {product.name}</h1>
-                <p className='text-sm'>Description: {product.description}</p>
-                <p className='text-sm'>Price: {product.price}</p>
-                <p className='text-sm'>Quantity: {product.quantity}</p>
-              </div>
+            <div className='flex-1 h-full'>
+              <img
+                src={product.imageUrl}
+                alt={product.name}
+                className='h-full w-full object-cover'
+              />
+            </div>
+            <div className='flex-1 p-4'>
+              <h1 className='text-xl font-semibold'>Name: {product.name}</h1>
+              <p className='text-sm'>Description: {product.description}</p>
+              <p className='text-sm'>Price: {product.price}</p>
+              <p className='text-sm'>Quantity: {product.quantity}</p>
             </div>
           </div>
         ))}
